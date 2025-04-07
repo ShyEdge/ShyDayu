312e922dfe7e8724253c850dc9b360da8c8188f1
ShyEdge <shyshy200309@gmail.com>
2025-04-07 20:05:41 +0800

    modify fixed agent

--- dependency/core/lib/algorithms/schedule_agent/fixed_agent.py
+++ dependency/core/lib/algorithms/schedule_agent/fixed_agent.py
@@ -28,8 +28,16 @@ class FixedAgent(BaseAgent, abc.ABC):
         pipe_seg = policy['pipeline']
         pipeline = info['pipeline']
         
-        pipeline = [{**p, 'execute_device': edge_device} for p in pipeline[:pipe_seg]] + \
-                   [{**p, 'execute_device': cloud_device} for p in pipeline[pipe_seg:]]
+        if(edge_device=='edge3'):
+            pipeline = [{**p, 'execute_device': 'edge3'} for p in pipeline[:1]] + \
+                    [{**p, 'execute_device': 'edge8'} for p in pipeline[1:]]
+        else:
+            pipeline = [{**p, 'execute_device': cloud_device} for p in pipeline[:1]] + \
+                    [{**p, 'execute_device': cloud_device} for p in pipeline[1:]]
+            
+
+
+
             
         self.sum_delay += self.last_delay
         self.cnt -= 1

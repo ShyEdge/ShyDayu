e1bff5f8e40ed931f7dbad91196f7dc89ed9fa94
ShyEdge <shyshy200309@gmail.com>
2025-04-18 14:49:09 +0800

    debug

--- dependency/core/lib/algorithms/parameter_monitor/bandwidth_monitor.py
+++ dependency/core/lib/algorithms/parameter_monitor/bandwidth_monitor.py
@@ -50,7 +50,7 @@ class BandwidthMonitor(BaseMonitor, abc.ABC):
         if self.is_server:
             return 0
 
-        @timeout(4)  #原始值是2
+        @timeout(8)  #原始值是2
         def fetch_bandwidth_by_iperf3():
             result = client.run()
             return result
